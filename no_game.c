bc7fb29b6781c848786dc815c5cde84647ac04dc
Asta9997 <gustavopubg0407@gmail.com>
2024-05-07 17:43:35 -0300

    opção de sair
    incluso no sistema a opção de saída para o usuário

--- no_game.c
+++ no_game.c
@@ -126,6 +126,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - Sair do Sistema\n\n");
 		printf("Opção:"); //fim do menu
 	
 		scanf("%d", &opcao); //armazenando a escolha do usuario
@@ -144,6 +145,11 @@ int main()
 			
 			case 3:
 			deletar(); //chamada de funções
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o Sistema!\n");
+			return 0;
 			break;
 			
 			default: //error(não didponivel)
